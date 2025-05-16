bdcd0b85306873150634dc8f5c88fecfa54a0836
Dinidu Maleesha <118754787+dinidumaleezha@users.noreply.github.com>
2025-05-16 19:17:32 +0530

    r1

---
+++ customtoastlib/src/main/java/com/maleezha/customtoastlib/ToastUtil.java
@@ -0,0 +1,29 @@
+package com.maleezha.customtoastlib;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ToastUtil {
+    public void showCustomToast(Context context, String message, int iconResId) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View layout = inflater.inflate(R.layout.custom_toast, null);
+
+        TextView text = layout.findViewById(R.id.toast_text);
+        ImageView icon = layout.findViewById(R.id.toast_icon);
+
+        text.setText(message);
+        icon.setImageResource(iconResId);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 100);
+        toast.show();
+    }
+
+}
